20d072babca76a09d796fe01326cbefbb4252fea
ALEJANDRO BAUTISTA MANCILLA <56178361+AbautistaPL@users.noreply.github.com>
2022-10-21 22:44:38 -0500

    - Finish worker.... - NEXT!!!! NOTIFICATIONS...

--- app/src/main/java/com/raiserdev/photogallery/PollWorker.kt
+++ app/src/main/java/com/raiserdev/photogallery/PollWorker.kt
@@ -4,14 +4,45 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.raiserdev.photogallery.model.PhotoRepository
+import kotlinx.coroutines.flow.first
+
 private const val TAG = "PollWorker"
 class PollWorker (
-    private val context: Context,
+    context: Context,
     workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters){
     override suspend fun doWork(): Result {
         Log.i(TAG, "Work request triggered.")
-        return Result.success()
+        val preferencesRepository = PreferencesRepository.get()
+        val photoRepository = PhotoRepository()
+
+        val query = preferencesRepository.storeQuery.first()
+        val lastId = preferencesRepository.lastResultId.first()
+        if (query.isEmpty()){
+            Log.i(TAG,"No saved query, finished early.")
+            return Result.success()
+        }
+
+        return try {
+            val items = photoRepository.searchPhotos(query = query)
+
+            if (items.isNotEmpty()){
+                val newResultId = items.first().id
+                if (newResultId == lastId){
+                    Log.i(TAG, "Still have the same result: $newResultId")
+                }else{
+                    Log.i(TAG,"Got a new result: $newResultId")
+                    preferencesRepository.setLastResultId(newResultId)
+                }
+            }
+            Result.success()
+
+        }catch (ex: Exception){
+            Log.e(TAG, "Background update failed", ex)
+            Result.failure()
+        }
+        //return Result.success()
     }
 
 }

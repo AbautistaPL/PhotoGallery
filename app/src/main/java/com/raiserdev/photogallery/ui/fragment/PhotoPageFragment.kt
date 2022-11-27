c2a8e2e685bbde3f7c3195b82d6885f84bac7d61
ALEJANDRO BAUTISTA MANCILLA <56178361+AbautistaPL@users.noreply.github.com>
2022-11-27 00:56:38 -0600

    - new URI WebView... - In progress...

--- app/src/main/java/com/raiserdev/photogallery/ui/fragment/PhotoPageFragment.kt
+++ app/src/main/java/com/raiserdev/photogallery/ui/fragment/PhotoPageFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.raiserdev.photogallery.databinding.FragmentPhotoPageBinding
 
 class PhotoPageFragment : Fragment() {
+    private val args: PhotoPageFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,35 @@ class PhotoPageFragment : Fragment() {
             false
         )
 
+        binding.apply {
+            progressBar.max = 100
+
+            webView.apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient(){
+                    override fun onProgressChanged(
+                        view: WebView?,
+                        newProgress: Int
+                    ) {
+                        if (newProgress == 100){
+                            progressBar.visibility = View.GONE
+                        }else{
+                            progressBar.visibility = View.VISIBLE
+                            progressBar.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(view: WebView?, title: String?) {
+                        val parent = requireActivity() as AppCompatActivity
+                        parent.supportActionBar?.subtitle = title
+                    }
+
+                }
+            }
+        }
         return binding.root
     }
 }
